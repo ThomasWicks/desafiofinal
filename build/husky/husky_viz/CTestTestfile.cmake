fe7788777f73ad541991cf42f68bc81951c22c85
ThomasWicks <thomas.wicks@hotmail.com>
2020-12-15 19:52:50 -0300

    initial

---
+++ build/husky/husky_viz/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/thomas/desafiofinal_ws/src/husky/husky_viz
+# Build directory: /home/thomas/desafiofinal_ws/build/husky/husky_viz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_husky_viz_roslaunch-check_launch "/home/thomas/desafiofinal_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python2" "/opt/ros/melodic/share/catkin/cmake/test/run_tests.py" "/home/thomas/desafiofinal_ws/build/test_results/husky_viz/roslaunch-check_launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/thomas/desafiofinal_ws/build/test_results/husky_viz" "/opt/ros/melodic/share/roslaunch/cmake/../scripts/roslaunch-check -o \"/home/thomas/desafiofinal_ws/build/test_results/husky_viz/roslaunch-check_launch.xml\" \"/home/thomas/desafiofinal_ws/src/husky/husky_viz/launch\" ")
